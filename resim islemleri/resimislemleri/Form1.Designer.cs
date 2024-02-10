e34604d887823f9d1a1158075ee90da2fe943759
Kaan <kaanc6286@gmail.com>
2024-02-10 20:43:28 +0300

    resim islemleri
    Windows Form ile tasarlandı
    C# ile yazıldı

---
+++ resim islemleri/resimislemleri/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace resimislemleri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(708, 443);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(727, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(52, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Resim Seç";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(727, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(52, 62);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Siyah Beyaz Yap";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(727, 132);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(52, 62);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "300 * 300 yap";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(727, 200);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(52, 62);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "1280 * 768 yap";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(727, 268);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(52, 62);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Rastgele renklerle doldur";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(727, 336);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(52, 46);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Kaydet";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(789, 467);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ resim islemleri/resimislemleri/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace resimislemleri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "resimler|*.BMP;*.JPG;*.GIF";
+            openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            pictureBox1.BackgroundImage = Image.FromFile(openFileDialog1.FileName);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap original = new Bitmap(pictureBox1.BackgroundImage);
+
+            Bitmap output = new Bitmap(pictureBox1.BackgroundImage.Width, pictureBox1.BackgroundImage.Height);
+
+            for (int i = 0; i < original.Width; i++)
+            {
+
+                for (int j = 0; j < original.Height; j++)
+                {
+
+                    Color c = original.GetPixel(i, j);
+
+                    int average = ((c.R + c.B + c.G) / 3);
+
+                    if (average < 200)
+                        output.SetPixel(i, j, Color.Black);
+
+                    else
+                        output.SetPixel(i, j, Color.White);
+
+                }
+            }
+
+            pictureBox1.BackgroundImage = output;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.BackgroundImage = ReSize(pictureBox1.BackgroundImage, new Size(300, 300));
+        }
+
+        public Image ReSize(Image resim, Size hedefBoyut)
+        {
+            Bitmap bmpResim = new Bitmap(hedefBoyut.Width, hedefBoyut.Height, PixelFormat.Format24bppRgb);
+            bmpResim.SetResolution(resim.HorizontalResolution, resim.VerticalResolution);
+
+            Graphics grResim = Graphics.FromImage(bmpResim);
+            grResim.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            grResim.FillRectangle(Brushes.White, 0, 0, hedefBoyut.Width, hedefBoyut.Height);
+
+            grResim.DrawImage(resim, new Rectangle(0, 0, hedefBoyut.Width, hedefBoyut.Height), new Rectangle(0, 0, resim.Width, resim.Height), GraphicsUnit.Pixel);
+            return bmpResim;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            pictureBox1.BackgroundImage = ReSize(pictureBox1.BackgroundImage, new Size(1280, 768));
+
+        }
+        Random rnd = new Random();
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Bitmap original = new Bitmap(pictureBox1.BackgroundImage);
+
+            Bitmap output = new Bitmap(pictureBox1.BackgroundImage.Width, pictureBox1.BackgroundImage.Height);
+
+            for (int i = 0; i < original.Width; i++)
+            {
+
+                for (int j = 0; j < original.Height; j++)
+                {
+                        output.SetPixel(i, j, Color.FromArgb(rnd.Next(0, 256), rnd.Next(200, 256), rnd.Next(200, 256)));
+                }
+            }
+
+            pictureBox1.BackgroundImage = output;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            try
+            {
+                pictureBox1.BackgroundImage.Save(saveFileDialog1.FileName);
+                MessageBox.Show("Resim Kaydedildi");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Kaydederken hata oluştu");
+            }
+        }
+    }
+}
